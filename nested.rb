22a9cd9c71cdd38934464b8d8917e19f5f97c817
Alex Marz <alexhmarz@gmail.com>
2020-11-03 17:42:16 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,7 +82,7 @@ def adding_matz
       }
     }
 
-adding_matz = programmer_hash[:yukihiro_matsumoto]
+adding_matz [:programmer_hash][:yukihiro_matsumoto]
   if programmer_hash[:yukihiro_matsumoto]
     programmer_hash << {:yukihiro_matsumoto=>{
   :known_for=> "Ruby",
